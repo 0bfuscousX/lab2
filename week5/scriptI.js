9f979d6842c2fa62c34f7149e01ddb9c3f7fcbee
Lorenz Tobias Lim <119904338+0bfuscousX@users.noreply.github.com>
2023-02-03 08:00:22 +0800

    javascript-update-flippingimage-1.2

--- week5/scriptI.js
+++ week5/scriptI.js
@@ -21,6 +21,5 @@ span.onclick = function() {
 }
   <img id="myImg" src="https://scontent.fmnl25-1.fna.fbcdn.net/v/t39.30808-6/281070084_1705209123162396_4163083173256478633_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFGJt4nDRmXT6k78YTa0QYPcbZWgt_WpCRxtlaC39akJNkEZuAGzhOP6UT22lUvXpKVtiXBfLZkHi-0f1fRZ78x&_nc_ohc=uGC4ZMtJmD8AX_Zwv1e&_nc_ht=scontent.fmnl25-1.fna&oh=00_AfBMmMV7j1PHryKfKWRtwv7uqFvjgP0kWkH1zsC3oz0tTA&oe=63D3FD9D" alt="ØbfuscousX" style="float:right;width:auto;height:450px;">
 	<!-- The Modal -->
-	 <div id="myModal" class="modal">
   	<span class="close">&times;</span>
   	 <img class="modal-content" id="img01">
